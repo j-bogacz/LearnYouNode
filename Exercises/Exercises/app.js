e7178912287cf894c0d844e65c0441a22163aa35
Jakub Bogacz <j.bogacz@outlook.com>
2015-12-27 18:26:38 +0100

    Exercise 8: Jakub

--- Exercises/Exercises/app.js
+++ Exercises/Exercises/app.js
@@ -1,10 +1,44 @@
 ﻿var http = require("http");
 var bl = require("bl");
 
+var responseCount = 0;
+var responses = ["", "", ""];
+
 http.get(process.argv[2], function (response) {
 	response.setEncoding("utf8");
 	response.pipe(bl(function (err, data) {
-		console.log(data.length);
-		console.log(data.toString());
+		responses[0] = data;
+		responseCount++;
+		if (responseCount === 3) {
+			printResult();
+		}
 	}));
 });
+
+http.get(process.argv[3], function (response) {
+	response.setEncoding("utf8");
+	response.pipe(bl(function (err, data) {
+		responses[1] = data;
+		responseCount++;
+		if (responseCount === 3) {
+			printResult();
+		}
+}));
+});
+
+http.get(process.argv[4], function (response) {
+	response.setEncoding("utf8");
+	response.pipe(bl(function (err, data) {
+		responses[2] = data;
+		responseCount++;
+		if (responseCount === 3) {
+			printResult();
+		}
+	}));
+});
+
+function printResult() {
+	responses.forEach(function (response) {
+		console.log(response.toString());
+	});
+}
