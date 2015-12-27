79a2d2642aa6f57ebc35eeaa158755afe620021f
Jakub Bogacz <j.bogacz@outlook.com>
2015-12-27 18:01:34 +0100

    Exercise 8

--- Exercises/Exercises/app.js
+++ Exercises/Exercises/app.js
@@ -1,9 +1,10 @@
 ﻿var http = require("http");
+var bl = require("bl");
 
 http.get(process.argv[2], function (response) {
 	response.setEncoding("utf8");
-	response.on("data", function (data) {
-		
-		console.log(data);
-	});
+	response.pipe(bl(function (err, data) {
+		console.log(data.length);
+		console.log(data.toString());
+	}));
 });
