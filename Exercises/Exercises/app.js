299593a576000e25a693ef54062a2a6a6ce413fb
Jakub Bogacz <j.bogacz@outlook.com>
2015-12-27 19:18:23 +0100

    Exercise 11: Jakub

--- Exercises/Exercises/app.js
+++ Exercises/Exercises/app.js
@@ -1,8 +1,12 @@
-﻿var net = require("net");
-var strftime = require("strftime");
+﻿var http = require("http");
+var fs = require("fs");
 
-var server = net.createServer(function (socket) {
-	socket.end(strftime("%F %R", new Date()) + "\n");
+var port = process.argv[2];
+var uri = process.argv[3];
+
+var server = http.createServer(function (request, response) {
+	stream = fs.createReadStream(uri);
+	stream.pipe(response);
 });
 
-server.listen(process.argv[2]);
+server.listen(port);
