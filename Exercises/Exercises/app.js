8a8b3806748a64899f3b68aa7963fa09b64b9e55
Jakub Bogacz <j.bogacz@outlook.com>
2015-12-27 19:11:10 +0100

    Exercise 10: Jakub

--- Exercises/Exercises/app.js
+++ Exercises/Exercises/app.js
@@ -1,28 +1,8 @@
-﻿var http = require("http");
-var bl = require("bl");
+﻿var net = require("net");
+var strftime = require("strftime");
 
-var responseCount = 0;
-var responses = [];
+var server = net.createServer(function (socket) {
+	socket.end(strftime("%F %R", new Date()) + "\n");
+});
 
-function httpGet(index) {
-	http.get(process.argv[2 + index], function (response) {
-		response.setEncoding("utf8");
-		response.pipe(bl(function (err, data) {
-			responses[index] = data;
-			responseCount++;
-			if (responseCount === 3) {
-				printResult();
-			}
-		}));
-	});
-}
-
-function printResult() {
-	for (var i = 0; i < 3; i++) {
-		console.log(responses[i].toString());
-	}
-}
-
-for (var i = 0; i < 3; i++) {
-	httpGet(i);
-}
+server.listen(process.argv[2]);
